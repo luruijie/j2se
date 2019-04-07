0deac03ea87394862a3bffd42d258eb5ad5bb17b
mouseJie <15627236898@163.com>
2019-04-07 15:23:23 +0800

    修改测试

--- src/main/java/com/mouse2java/App.java
+++ src/main/java/com/mouse2java/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
         try {
+            System.out.println("修改测试");
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("success!!!");
         }catch (Exception e){
